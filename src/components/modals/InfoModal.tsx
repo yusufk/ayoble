e969509b0b5d88760ce969697d2d2be4aea9797d
Yusuf Kaka <yusuf.kaka@mtn.com>
2022-03-17 14:26:57 +0200

    Added tracking

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -58,14 +58,8 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       </p>
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love -{' '}
-        <a
-          href="https://github.com/cwackerfuss/react-wordle"
-          className="underline font-bold"
-        >
-          check out the code here
-        </a>{' '}
+        This is the ayoba version of the word guessing game we all know and
+        love, forked from react-wordle -{' '}
       </p>
     </BaseModal>
   )
