5d40d57f9827ac131ed2f5387fc3b80c6d001fea
Yusuf Kaka <yusuf.kaka@mtn.com>
2022-03-18 11:07:10 +0200

    forgot to commit initial contact form

--- src/components/modals/ContactModal.tsx
+++ src/components/modals/ContactModal.tsx
@@ -9,7 +9,25 @@ export const ContactModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="Contact us" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        If you are experiencing any issues with the game, please contact us at yusufk@mailbox.co.za
+        Fill in the form below to contact the developer:
+      </p>
+
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        <form action="mailto:yusufk@mailbox.co.za" method="POST" encType="multipart/form-data" name="EmailForm">
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input type="email" id="email" required />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea id="message" required />
+        </div>
+        <button type="submit">Submit</button>
+        </form>
       </p>
     </BaseModal>
   )
