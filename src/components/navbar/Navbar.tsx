848d7be7aff677313f752334d02fdb6b3431f27f
Yusuf Kaka <yusuf.kaka@mtn.com>
2022-03-18 11:02:15 +0200

    Added contact modal

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@ import {
   ChartBarIcon,
   CogIcon,
   InformationCircleIcon,
+  MailIcon,
 } from '@heroicons/react/outline'
 import { GAME_TITLE } from '../../constants/strings'
 
@@ -9,20 +10,28 @@ type Props = {
   setIsInfoModalOpen: (value: boolean) => void
   setIsStatsModalOpen: (value: boolean) => void
   setIsSettingsModalOpen: (value: boolean) => void
+  setIsContactModalOpen: (value: boolean) => void
 }
 
 export const Navbar = ({
   setIsInfoModalOpen,
   setIsStatsModalOpen,
   setIsSettingsModalOpen,
+  setIsContactModalOpen,
 }: Props) => {
   return (
     <div className="navbar">
       <div className="navbar-content px-5">
+      <div className="right-icons">
         <InformationCircleIcon
           className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
           onClick={() => setIsInfoModalOpen(true)}
         />
+        <MailIcon
+          className="h-6 w-6 mr-2 cursor-pointer dark:stroke-white"
+          onClick={() => setIsContactModalOpen(true)}
+        />
+        </div>
         <p className="text-xl ml-2.5 font-bold dark:text-white">{GAME_TITLE}</p>
         <div className="right-icons">
           <ChartBarIcon
