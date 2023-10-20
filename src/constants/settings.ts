3f3114de249baf3709789b343e5f485628789ada
Yusuf Kaka <yusufk@gmail.com>
2023-10-20 18:25:26 +0200

    ayobafied colours

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,4 +1,4 @@
-import { enUS } from 'date-fns/locale'
+import { enZA } from 'date-fns/locale'
 
 export const MAX_CHALLENGES = 6
 export const ALERT_TIME_MS = 2000
@@ -10,4 +10,4 @@ export const ENABLE_MIGRATE_STATS = true
 export const BLOWFISH_KEY = 'xcQUAHsik#Thq&LG*8es2DsZ$3bw^e'
 export const BLOWFISH_IV = '#45XmF^w'
 export const ENABLE_ARCHIVED_GAMES = false
-export const DATE_LOCALE = enUS
+export const DATE_LOCALE = enZA
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
         xshort: { raw: '(max-height: 560px)' },
         xxshort: { raw: '(max-height: 490px)' },
       },
+      colors: {
+        'ayoba_blue': '#146EA6',
+        'ayoba_dark_blue': '#034C8C',
+        'ayoba_grey': '#F2F2F2',
+      },
     },
   },
   plugins: [require('@tailwindcss/forms')],
